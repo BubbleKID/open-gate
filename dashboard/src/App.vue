8f085b93a86f7e87ee51a2ac3a5d48ec9e143b1e
Mark <>
2020-12-04 12:42:02 +1100

    add login

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width=120>
-    <Table msg="Welcome to Your Vue.js App"/>
+    <img v-if="this.$store.state.isLogin" alt="Vue logo" src="./assets/logo.png" width=120>
+    <Table v-if="this.$store.state.isLogin" msg="Welcome to Your Vue.js App"/>
+    <Login v-if="!this.$store.state.isLogin"/>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import Vuex from 'vuex';
 import Table from './components/Table.vue';
+import Login from './components/Login.vue';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    isLogin: false,
+  },
+  mutations: {
+    userLogin(state) {
+      state.isLogin = !state.isLogin;
+      localStorage.setItem('isLogin', state.isLogin);
+    },
+  },
+});
 
 export default {
   name: 'App',
+  store,
   components: {
     Table,
+    Login,
+  },
+  mounted() {
+    if (localStorage.getItem('isLogin')) {
+      this.$store.state.isLogin = localStorage.getItem('isLogin');
+    }
   },
 };
 </script>
@@ -23,6 +48,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
